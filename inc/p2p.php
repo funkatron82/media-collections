17370e4f217e1576e5cfb7818550805e843cfa88
Manny Fleurmond <funkatronic@gmail.com>
2014-11-03 08:06:13 -0600

    Refactored P2P loading code

--- inc/p2p.php
+++ inc/p2p.php
@@ -4,17 +4,23 @@ if( !defined( 'P2P_TEXTDOMAIN' ) )
 	
 require CEDMC_INC_DIR . '/scb/load.php';
 
-scb_init( 'cedmc_p2p_core_init' );
-
-function cedmc_p2p_core_init() {
-	add_action( 'plugins_loaded', 'cedmc_load_p2p_core', 20 );
-}
-
 function cedmc_load_p2p_core() {
-	if ( function_exists( 'p2p_register_connection_type' ) )
-		return;
+	if ( !function_exists( 'p2p_register_connection_type' ) ) {
+		load_plugin_textdomain( P2P_TEXTDOMAIN, '', basename( dirname( __FILE__ ) ) . '/lang' );
+		require_once CEDMC_INC_DIR . '/p2p-core/autoload.php';
 
-	require_once CEDMC_INC_DIR . '/p2p-core/init.php';
-
-	add_action( 'admin_init', array( 'P2P_Storage', 'install' ) );
+		P2P_Storage::init();
+	
+		P2P_Query_Post::init();
+		P2P_Query_User::init();
+	
+		P2P_URL_Query::init();
+	
+		P2P_Widget::init();
+		P2P_Shortcodes::init();
+	
+		register_uninstall_hook( __FILE__, array( 'P2P_Storage', 'uninstall' ) );
+	}
 }
+
+scb_init( 'cedmc_load_p2p_core' );
